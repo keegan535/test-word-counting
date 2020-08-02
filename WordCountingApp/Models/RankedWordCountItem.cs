9521f2ece319e0db5ec16ed25bbda5df5c4a1779
Keegan <keegan535@gmail.com>
2020-08-02 12:58:26 +0100

    init commit - create basic mvvm app

---
+++ WordCountingApp/Models/RankedWordCountItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordCountingApp.Models
+{
+    class RankedWordCountItem
+    {
+        public int Rank { get; set; }
+
+        public string Word { get; set; }
+
+        public int Count { get; set; }
+
+        public RankedWordCountItem(int rank, string word, int count)
+        {
+            Rank = rank;
+            Word = word;
+            Count = count;
+        }
+
+    }
+}
---
+++ WordCountingApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using WordCountingApp.Models;
+
+namespace WordCountingApp.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<RankedWordCountItem> wordCountItems;
+
+        public ObservableCollection<RankedWordCountItem> WordCountItems
+        {
+            get => wordCountItems;
+            set => wordCountItems = value;
+        }
+
+        public MainWindowViewModel()
+        {
+            WordCountItems = new ObservableCollection<RankedWordCountItem>();
+        }
+
+        #region INotifyPropertyChanged Implementations
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
